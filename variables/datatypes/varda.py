702661280d8545058f106ae34c2d74e52929b466
Subomi Obans <subomi1@users.noreply.github.com>
2025-08-06 13:52:53 +0100

    learning about the inout model

--- variables/datatypes/varda.py
+++ variables/datatypes/varda.py
@@ -1,29 +1,46 @@
-#  single line comment
+# #  single line comment
 
-# ::: variables
-firstName = "subomi"
-lastName = "obans"
-age = 22
+# # ::: variables
+# firstName = "subomi"
+# lastName = "obans"
+# age = 22
 
-print(firstName)
-print(lastName)
-print(age)
+# print(firstName)
+# print(lastName)
+# print(age)
 
-# ::: data types
+# # ::: data types
 
-# integer
-age = 25
-print(type(age))
+# # integer
+# age = 25
+# print(type(age))
 
-#  float
-height = 5.7
-print(type(height))
+# #  float
+# height = 5.7
+# print(type(height))
 
-# string
-full_name = "obans olasubomi"
-print(type(full_name))
+# # string
+# full_name = "obans olasubomi"
+# print(type(full_name))
 
-#  bool .. T(True)/F(False)
-is_good = True
-print(type(is_good))
+# #  bool .. T(True)/F(False)
+# is_good = True
+# print(type(is_good))
+
+# ::: how to accept input
+# first_name = input("Enter your first name: ")
+# print(first_name)
+
+# In Python, the input() function always returns a string, even if the user types a number.
+# To use it as an integer, you need to convert (cast) it with int().
+
+# ::: Example
+age = input("Enter your age: ")
+print(type(age))  # this will be a string <class 'str'>
+
+age = int(input("Enter your age: "))
+print(type(age)) # this will be an integer <class 'int'>
+
+
+
 
