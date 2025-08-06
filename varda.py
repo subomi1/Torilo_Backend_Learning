b6ddee82231392e8fa9bebb8d8d7c82d2cfa71f8
Subomi Obans <subomi1@users.noreply.github.com>
2025-08-06 12:25:43 +0100

    variables & dataTypes(put that pascal case for efisi)

--- varda.py
+++ varda.py
@@ -9,3 +9,21 @@ print(firstName)
 print(lastName)
 print(age)
 
+# ::: data types
+
+# integer
+age = 25
+print(type(age))
+
+#  float
+height = 5.7
+print(type(height))
+
+# string
+full_name = "obans olasubomi"
+print(type(full_name))
+
+#  bool .. T(True)/F(False)
+is_good = True
+print(type(is_good))
+
